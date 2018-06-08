077b4dedf5a32fdfbd620547751342e71dde8a25
Joe Hart <joehart42@gmail.com>
2018-06-08 23:20:06 +0100

    Used actual equation.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {
 } from 'postprocessing';
 import * as THREE from 'three';
 import Stats from 'stats-js';
+import dat from 'dat.gui';
 import './style.css';
 
 // global objects
@@ -13,8 +14,18 @@ let scene;
 let renderer;
 let composer;
 let camera;
-const seeds = [];
-let turnFraction = 16 / 10;
+let seeds;
+let started = true;
+const gui = new dat.GUI();
+const config = {
+  seedSpacing: 17,
+  turnFractionTop: 16,
+  turnFractionBottom: 10,
+  speed: 5,
+  degrees: (16 / 10) * 360,
+  turnFraction: 16 / 10,
+  numberOfSeeds: 1500,
+};
 
 function setUpStats() {
   stats = new Stats();
@@ -32,6 +43,7 @@ function setUpScene() {
 
 function setUpRenderer() {
   const camFactor = 2;
+
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(window.innerWidth / window.innerHeight);
@@ -66,75 +78,126 @@ function setUpRenderer() {
   });
 }
 
-function renderWindow() {
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setPixelRatio(window.devicePixelRatio);
-  return renderer.domElement;
-}
-
 function getPositionOfSeed(num) {
   // console.log(`GETTING POSITION OF SEED ${num}`);
-  const spacing = 1;
-  const turnAmount = 360 * turnFraction;
-  const totalTurn = num * turnAmount;
+  // const turnAmount = 360 * config.turnFraction;
+  // const totalTurn = num * turnAmount;
   // console.log(`TOTAL TURN: ${totalTurn}`);
-  const distance = ((totalTurn / 360) * spacing) + spacing;
+  // const distance = ((totalTurn / 360) * config.seedSpacing * 0.5) + config.seedSpacing;
   // console.log(`DISTANCE: ${distance}`);
-  const theta = totalTurn;
+  // const theta = totalTurn;
   // console.log(`THETA: ${theta}`);
-  const thetaRadian = theta * (Math.PI / 180);
+  const distance1 = (config.seedSpacing * Math.sqrt(num + 1));
+  const distance2 = -distance1;
+  // const theta = num * (config.degrees / (Math.PI * 180));
+  const theta = num * (2 * Math.PI * config.turnFraction);
+
+
+  // const thetaRadian = theta * (Math.PI / 180);
   // console.log(`THETA RADIAN: ${theta}`);
   return {
-    x: distance * Math.cos(thetaRadian),
-    y: distance * Math.sin(thetaRadian),
+    x1: distance1 * Math.cos(theta),
+    y1: distance1 * Math.sin(theta),
+    x2: distance2 * Math.cos(theta),
+    y2: distance2 * Math.sin(theta),
   };
+}
+
+function deleteSeeds() {
+  seeds.map((seed) => {
+    scene.remove(seed);
+    return null;
+  });
 }
 
 // particle set up
 function setUpSeeds() {
-  const sphereCount = 1000;
+  seeds = [];
   const sphereGeometry = new THREE.SphereGeometry(5, 10, 8);
   const bufferSphereGeometry = new THREE.BufferGeometry()
     .fromGeometry(sphereGeometry);
 
-  for (let i = 0; i < sphereCount; i++) { // eslint-disable-line
+  for (let i = 0; i < config.numberOfSeeds; i += 2) { // eslint-disable-line
     const object = new THREE.Mesh(
       bufferSphereGeometry,
-      new THREE.PointsMaterial({ color: Math.random() * 0xffffff }),
+      new THREE.PointsMaterial({ color: 0x00ff00 }),
+    );
+    const object2 = new THREE.Mesh(
+      bufferSphereGeometry,
+      new THREE.PointsMaterial({ color: 0xff0000 }),
     );
     const coords = getPositionOfSeed(i);
-    object.position.x = coords.x;
-    object.position.y = coords.y;
+    object.position.x = coords.x1;
+    object.position.y = coords.y1;
     object.position.z = -100;
+    object2.position.x = coords.x2;
+    object2.position.y = coords.y2;
+    object2.position.z = -100;
     seeds.push(object);
     scene.add(object);
+    seeds.push(object2);
+    scene.add(object2);
   }
+  console.log(seeds);
 }
 
 function animate() {
   stats.begin();
   requestAnimationFrame(animate);
-  seeds.map((seed, index) => {
-    const coords = getPositionOfSeed(index, turnFraction);
-    seed.position.x = coords.x;
-    seed.position.y = coords.y;
-    return seed;
-  });
-  turnFraction += 0.000001;
+  if (started) {
+    for (let i = 0; i < config.numberOfSeeds; i += 2) {
+      const coords = getPositionOfSeed(i);
+      seeds[i].position.x = coords.x1;
+      seeds[i].position.y = coords.y1;
+
+      if ((i + 1) < config.numberOfSeeds) {
+        seeds[i + 1].position.x = coords.x2;
+        seeds[i + 1].position.y = coords.y2;
+      }
+    }
+    config.turnFractionTop += 0.00001 * config.speed;
+    config.turnFraction = config.turnFractionTop / config.turnFractionBottom;
+  }
   composer.render();
   stats.end();
 }
 
+function setUpUI() {
+  // add three render to window
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  document.body.appendChild(renderer.domElement);
+  gui.add(config, 'turnFractionTop', 0, 25);
+  gui.add(config, 'turnFractionBottom', 0, 25);
+  gui.add(config, 'seedSpacing', 0, 100);
+  const fractionControls = gui.add(config, 'turnFraction')
+    .step(0.00001);
+  fractionControls.onChange(() => {
+    started = false;
+  });
+  fractionControls.onFinishChange(() => {
+    started = true;
+  });
+  gui.add(config, 'speed', 0, 10);
+  const numberOfSeedControls = gui.add(config, 'numberOfSeeds', 0, 3000);
+  numberOfSeedControls.onChange(() => {
+    started = false;
+  });
+  numberOfSeedControls.onFinishChange(() => {
+    deleteSeeds();
+    setUpSeeds();
+    started = true;
+  });
+}
 
 function init() {
   // Set Up Stats
   setUpStats();
   setUpScene();
   setUpRenderer();
+  setUpUI();
   setUpSeeds();
-  document.body.appendChild(renderWindow());
 }
-
 
 init();
 animate();
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  devtool: 'inline-source-map',
+  devtool: 'cheap-eval-source-map',
   devServer: {
     contentBase: './dist',
     hot: true,
