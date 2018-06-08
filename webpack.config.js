e0b172b4abcf348af605762b64b6e54102f6af3e
Joe Hart <joehart42@gmail.com>
2018-06-08 18:14:45 +0100

    webpack and eslint set up.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPplugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
   plugins: [
     new CleanWebpackPplugin(['dist']),
     new HtmlWebpackPlugin({
-        title: "Golden Flowers",
-    })
+      title: 'Golden Flowers',
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new UglifyJsPlugin(),
   ],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
 };
